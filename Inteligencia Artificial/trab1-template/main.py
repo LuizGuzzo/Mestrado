e9b8bfe7a970db7a5afb02ed3974a99c29221b8b
Luiz <luizguzzo@gmail.com>
2021-11-18 17:00:11 -0300

    codigo finalizado

--- Inteligencia Artificial/trab1-template/main.py
+++ Inteligencia Artificial/trab1-template/main.py
@@ -12,59 +12,96 @@ def main():
     maze_problem = MazeProblem(n_rows=20, n_cols=20, seed=None) # seed = none ira gerar uma seed aleatoria e ira printa-la
     viewer = MazeViewer(maze_problem, step_time_miliseconds=20, zoom=20)
 
+    resultado = []
 
-    data = []
-    start = time.time()
-    path, cost = breadth_first_search(maze_problem, viewer)
-    end = time.time()
-    timer = end - start
-    data.append([len(path),cost,timer])
-    show_table(data,"breadth")
+    timers = []
+    for _ in range(10):    
+        start = time.time()
+        path, cost = breadth_first_search(maze_problem, viewer)
+        end = time.time()
+        timer = end - start
+        timers.append(timer)
+
     viewer.update(path=path)
     viewer.pause()
+    avgtime = show_table(timers,"breadth")
+    if len(path) == 0:
+        print("Goal is unreachable for this maze.")
+    print(f"Uniform Path cost: {cost}. Number of steps: {len(path)-1}. Time: {avgtime}")
 
-    data = []
-    start = time.time()
-    path, cost = A_Pathfinding(maze_problem, viewer)
-    end = time.time()
-    timer = end - start
-    data.append([len(path),cost,timer])
-    show_table(data,"A*")
+    resultado.append(["breadth",cost,len(path)-1,avgtime])
+
+    timers = []
+    for _ in range(10):    
+        start = time.time()
+        path, cost = A_Pathfinding(maze_problem, viewer)
+        end = time.time()
+        timer = end - start
+        timers.append(timer)
+
     viewer.update(path=path)
     viewer.pause()
+    avgtime = show_table(timers,"A*")
+    if len(path) == 0:
+        print("Goal is unreachable for this maze.")
+    print(f"Uniform Path cost: {cost}. Number of steps: {len(path)-1}. Time: {avgtime}")
 
-    data = []
-    start = time.time()
-    path, cost = depth_first_search(maze_problem, viewer)
-    end = time.time()
-    timer = end - start
-    data.append([len(path),cost,timer])
-    show_table(data,"Depth")
+    resultado.append(["A*     ",cost,len(path)-1,avgtime])
+
+    timers = []
+    for _ in range(10):    
+        start = time.time()
+        path, cost = depth_first_search(maze_problem, viewer)
+        end = time.time()
+        timer = end - start
+        timers.append(timer)
+
     viewer.update(path=path)
     viewer.pause()
+    avgtime = show_table(timers,"Depth")
+    if len(path) == 0:
+        print("Goal is unreachable for this maze.")
+    print(f"Uniform Path cost: {cost}. Number of steps: {len(path)-1}. Time: {avgtime}")
 
-    data = []
-    start = time.time()
-    path, cost = uniform_cost_search(maze_problem, viewer)
-    end = time.time()
-    timer = end - start
-    data.append([len(path),cost,timer])
-    show_table(data,"Uniform")
+    resultado.append(["Depth  ",cost,len(path)-1,avgtime])
+
+    timers = []
+    for _ in range(10):    
+        start = time.time()
+        path, cost = uniform_cost_search(maze_problem, viewer)
+        end = time.time()
+        timer = end - start
+        timers.append(timer)
+
     viewer.update(path=path)
     viewer.pause()
+    avgtime = show_table(timers,"Uniform")
+    if len(path) == 0:
+        print("Goal is unreachable for this maze.")
+    print(f"Uniform Path cost: {cost}. Number of steps: {len(path)-1}. Time: {avgtime}")
+
+    resultado.append(["Uniform",cost,len(path)-1,avgtime])
+
+    print("Tipo: \t Path Cost: \t Steps: \t Time:")
+    print("-----------------------------------------------")
+    for (tipo,cost,path,avgtime) in resultado:
+        print(f"{tipo} \t {cost:.2f} \t {path} \t {avgtime:.4f}")
 
     print("OK!")
 
 def show_table(dados,type):
+    time = 0
+    print("")
     print("tipo usado:",type)
-    print("Path cost: \t Number of steps: \t Time: \t ")
-    print("-------------------------------------------------------")
-    for (cost,path,timer) in dados:
-        if path == 0:
-            print("Goal is unreachable for this maze.")
-        else:
-            #print("%f \t %d \t %f" % (cost,path-1,timer))
-            print("{} \t {} \t {}".format(cost,path-1,timer))
+    print("\t Times: \t ")
+    print("---------------------------------------")
+    for timer in dados:
+        time += timer
+        print("\t {} \t".format(timer))
+
+    print("Tempo medio:", time/10)
+    print("---------------------------------------")
+    return time/10
 
 if __name__ == "__main__":
     main()
